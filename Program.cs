bb1678f50622cbbd3693d7681d149905140f1057
ZavarkoZeno <zavarkoz@kkszki.hu>
2024-10-04 13:28:09 +0200

    Project is ready

--- Program.cs
+++ Program.cs
@@ -11,13 +11,23 @@ namespace Szamrendszerek
         static void Main(string[] args)
         {
             int szam = 0;
-            Console.WriteLine("Adjon meg egy 10-es számrendszerbeli számot: ");
+            Console.WriteLine("Írj be egy 10-es számrendszerben lévő számot: ");
             szam = Convert.ToInt32(Console.ReadLine());
 
-            while (szam % 2 == 0)
+            while (szam <= 0)
             {
+                if (szam % 2 == 0)
+                {
+                    
+                    szam <= 1;
+                }
 
+                else
+                {
+
+                }
             }
+            Console.WriteLine(); ;
         }
     }
 }
